1dc3d39f920141d84b725fdf6144d1a29b51f3b9
branden-nguyen24 <30638422+branden-nguyen24@users.noreply.github.com>
2023-12-13 14:35:06 -0800

    tested nlp text classification model for sentiment

---
+++ sentiment.py
@@ -0,0 +1,83 @@
+import json
+import tensorflow as tf
+import numpy as np
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+training_size = 20000
+
+with open("sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+
+# tokenizer = Tokenizer(oov_token="<OOV>")
+# tokenizer.fit_on_texts(sentences)
+# word_index = tokenizer.word_index
+#
+# sequences = tokenizer.texts_to_sequences(sentences)
+# padded = pad_sequences(sequences, padding='post')
+# print(word_index)
+# print(padded[0])
+# print(padded.shape)
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+# Need this block to get it to work with TensorFlow 2.x
+training_padded = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_padded = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+
+# neural network embedding
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+num_epochs = 30
+
+# sentiment model training
+history = model.fit(training_padded, training_labels, epochs=num_epochs, validation_data=(testing_padded, testing_labels), verbose=2)
+
+sentence = [
+    "granny starting to fear spiders in the garden might be real",
+    "the weather today is bright and sunny"
+    ]
+
+sequences = tokenizer.texts_to_sequences(sentence)
+
+padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+print(model.predict(padded))
